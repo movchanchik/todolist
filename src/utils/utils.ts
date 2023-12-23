9efbab788a38a839a71829674d9ef16a9f31112f
movchanchik <inna0992@gmail.com>
2023-12-22 20:03:50 -0500

    fix type name

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,12 +1,10 @@
 import { getListTodos } from "../utils/todoApi";
-import { TodoItemType } from "../types/todo";
+import { TodoType } from "../types/todo";
 
 export const checkUniq = async (value: string) => {
   try {
     const todos = await getListTodos(false);
-    const filteredTodos = todos.filter(
-      (todo: TodoItemType) => todo.todo !== value
-    );
+    const filteredTodos = todos.filter((todo: TodoType) => todo.todo !== value);
     return todos.length === filteredTodos.length;
   } catch (error) {
     console.log(error);
